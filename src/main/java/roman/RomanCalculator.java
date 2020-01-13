178d1cca65c3cf5833fc6ee456a658f8c8c3ea28
ericli <eric.q.f.li@oocl.com>
2020-01-13 19:59:43 +0800

    [eric][implement]: should_return_XII_when_given_roman1_is_II_and_roman2_is_X

--- src/main/java/roman/RomanCalculator.java
+++ src/main/java/roman/RomanCalculator.java
@@ -1,7 +1,8 @@
 package roman;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
+
+import static javax.swing.UIManager.put;
 
 public class RomanCalculator {
 
@@ -17,9 +18,42 @@ public class RomanCalculator {
         }
     };
 
+    private static final Map<Integer, String> intMap = new LinkedHashMap<Integer, String>() {
+        {
+            put(1000, "M");
+            put(500, "D");
+            put(100, "C");
+            put(50, "L");
+            put(10, "X");
+            put(5, "V");
+            put(1, "I");
+        }
+    };
+
     public String add(String input1, String input2) {
-        if (romanMap.get(input1) >= romanMap.get(input2))
-            return input1 + input2;
-        return input2 + input1;
+        int number1 = convertRomanToNumber(input1);
+        int number2 = convertRomanToNumber(input2);
+        return convertNumberToRoman(number1 + number2);
+    }
+
+    private String convertNumberToRoman(int number) {
+        StringBuilder result = new StringBuilder();
+        Set<Integer> intMapKey = intMap.keySet();
+        for (Integer i : intMapKey) {
+            int num = number / i;
+            while (num-- != 0) {
+                result.append(intMap.get(i));
+                number -= i;
+            }
+        }
+        return result.toString();
+    }
+
+    private int convertRomanToNumber(String roman) {
+        int result = 0;
+        for (int i = 0; i < roman.length(); ++i) {
+            result += romanMap.get(String.valueOf(roman.charAt(i)));
+        }
+        return result;
     }
 }

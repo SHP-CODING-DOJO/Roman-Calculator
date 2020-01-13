1eeeb9f2ec78d1046931ae67a372288d4e993e31
ericli <eric.q.f.li@oocl.com>
2020-01-13 20:14:47 +0800

    [eric][implement]: should_return_IV_when_given_roman1_is_II_and_roman2_is_II

--- src/main/java/roman/RomanCalculator.java
+++ src/main/java/roman/RomanCalculator.java
@@ -21,11 +21,17 @@ public class RomanCalculator {
     private static final Map<Integer, String> intMap = new LinkedHashMap<Integer, String>() {
         {
             put(1000, "M");
+            put(900, "CM");
             put(500, "D");
+            put(400, "CD");
             put(100, "C");
+            put(90, "XC");
             put(50, "L");
+            put(40, "XL");
             put(10, "X");
+            put(9, "IX");
             put(5, "V");
+            put(4, "IV");
             put(1, "I");
         }
     };

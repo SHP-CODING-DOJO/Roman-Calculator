e7a696cff1faae52577941d651ab167cfa5a049d
ericli <eric.q.f.li@oocl.com>
2020-01-13 19:13:59 +0800

    [barry][test]: should_return_II_when_given_roman1_is_I_and_roman2_is_I

--- src/main/java/roman/RomanCalculator.java
+++ src/main/java/roman/RomanCalculator.java
@@ -1,4 +1,8 @@
 package roman;
 
 public class RomanCalculator {
+
+    public String add(String input1, String input2) {
+        return null;
+    }
 }
--- src/test/java/roman/RomanCalculatorTest.java
+++ src/test/java/roman/RomanCalculatorTest.java
@@ -1,11 +1,39 @@
 package roman;
 
-import org.junit.Assert;
-import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
 
+import java.util.Arrays;
+import java.util.Collection;
+
+import static org.junit.Assert.assertEquals;
+
+@RunWith(Parameterized.class)
 public class RomanCalculatorTest {
-    @Before
-    public void setUp() throws Exception {
+
+    private String input1;
+    private String input2;
+    private String expected;
+
+    @Parameterized.Parameters
+    public static Collection prepareData() {
+        Object[][] data = {
+                {"I", "I", "II"}
+        };
+        return Arrays.asList(data);
+    }
+
+    public RomanCalculatorTest(String input1, String input2, String expected) {
+        this.input1 = input1;
+        this.input2 = input2;
+        this.expected = expected;
+    }
+
+    @Test
+    public void testAdd() {
+        RomanCalculator romanCalculator = new RomanCalculator();
+        String actual = romanCalculator.add(input1, input2);
+        assertEquals(expected, actual);
     }
 }
